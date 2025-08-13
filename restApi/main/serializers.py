596e977c928e8185daa54cb8703e22d4ab5b2c4a
John Victor <johnvictorxavierr@gmail.com>
2025-08-13 09:46:31 -0300

    [ajuste - logica - completa] (Removendo dataInicio TarefasSerializer) Removendo o campo de inicio de tarefa e título do TarefasSerializer

--- restApi/main/serializers.py
+++ restApi/main/serializers.py
@@ -74,10 +74,9 @@ class TarefasSerializer(serializers.ModelSerializer):
     advogadoCriadorNome = serializers.CharField(source='advogadoCriadorId.nome',read_only=True)
     advogadoResponsavelNome = serializers.CharField(source='advogadoResponsavelId.nome',read_only=True)
     clienteNome = serializers.CharField(source='processoOrigemId.clienteId.nome',read_only=True)
-    dataInicio = serializers.DateTimeField(format="%d/%m/%Y ")
     prazoFinal = serializers.DateTimeField(format="%d/%m/%Y ")
     class Meta:
         model = Tarefas
-        fields = ['id','titulo','descricao','prazoFinal','status','processoOrigemId','processoOrigemNumero','advogadoCriadorId','advogadoCriadorNome','advogadoResponsavelId','advogadoResponsavelNome','clienteNome','urgente','concluida','observacoes']
+        fields = ['id','descricao','prazoFinal','status','processoOrigemId','processoOrigemNumero','advogadoCriadorId','advogadoCriadorNome','advogadoResponsavelId','advogadoResponsavelNome','clienteNome','urgente','concluida','observacoes']
         # sem dataInicio pq é auto now add
         
