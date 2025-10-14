6bafee65cfb831b60662fe3b9ff03410c16c87a0
ThalesHenri <thales1337@hotmail.com>
2025-10-14 11:28:29 -0300

    [Ajustes] (Models) Ajeitando o Foreing Key entre clientes e representantes(ajustei o serializers para não dar conflito).

--- restApi/main/serializers.py
+++ restApi/main/serializers.py
@@ -31,8 +31,8 @@ class ClienteSerializer(serializers.ModelSerializer):
 
 class RepresentanteSerializer(serializers.ModelSerializer):
     endereco = serializers.SerializerMethodField(method_name='get_endereco')
-    cliente_id = serializers.PrimaryKeyRelatedField(many=True, read_only=True,source='cliente_set')
-    clientes = serializers.SerializerMethodField(method_name='get_clientes')
+    cliente_id = serializers.PrimaryKeyRelatedField(read_only=True)
+    cliente_info = serializers.SerializerMethodField(method_name='get_clientes')
     class Meta:
         model = Representante
         fields = '__all__'
@@ -50,8 +50,11 @@ class RepresentanteSerializer(serializers.ModelSerializer):
         return endereco
     
     def get_clientes(self,obj):
-        clientes = obj.cliente_set.all()
-        return [{'cliente_id': cliente.id, 'cliente_nome': cliente.nome} for cliente in clientes]
+        if obj.cliente:
+            return {
+                'id': obj.cliente.id,
+                'nome': obj.cliente.nome,
+            }
         
         
         
