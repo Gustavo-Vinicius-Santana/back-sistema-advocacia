dcb70f2aef2ebd46735aff8ca6aef6a22381d900
Gustavo Vinicius <gus.tec.dev@gmail.com>
2026-08-18 19:48:05 -0300

    (fix - deploy): corrigindo logica de logout

--- restApi/main/novas_views/advogados_views.py
+++ restApi/main/novas_views/advogados_views.py
@@ -206,17 +206,9 @@ class AdvogadoLogoutView(APIView):
                 pass
 
         response = Response({"detail": "Logout realizado com sucesso."})
-        # Deletar cookies com os mesmos parâmetros usados na criação
-        samesite_value = settings.JWT_COOKIE_SAMESITE if settings.JWT_COOKIE_SAMESITE != 'None' else None
-        cookie_kwargs = {
-            'path': '/',
-            'secure': settings.JWT_COOKIE_SECURE,
-        }
-        if samesite_value:
-            cookie_kwargs['samesite'] = samesite_value
-
-        response.delete_cookie('access_token', **cookie_kwargs)
-        response.delete_cookie('refresh_token', **cookie_kwargs)
+        # Deletar cookies - em cross-origin eles estão no domínio da API
+        response.delete_cookie('access_token', path='/', secure=settings.JWT_COOKIE_SECURE)
+        response.delete_cookie('refresh_token', path='/', secure=settings.JWT_COOKIE_SECURE)
         return response
 
 
