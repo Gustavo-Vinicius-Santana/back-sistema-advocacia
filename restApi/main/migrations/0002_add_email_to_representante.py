1584ffe0448cbbb1c384dd5130ab1b575d146a43
Gustavo Vinicius <gus.tec.dev@gmail.com>
2026-08-18 20:59:36 -0300

    (fix - deploy): corrigindo bug de migration

--- restApi/main/migrations/0002_add_email_to_representante.py
+++ restApi/main/migrations/0002_add_email_to_representante.py
@@ -1,4 +1,4 @@
-# Generated migration to add email field to Representante model
+# Generated migration to add missing fields to Representante model
 
 from django.db import migrations, models
 
@@ -14,5 +14,20 @@ class Migration(migrations.Migration):
             model_name='representante',
             name='email',
             field=models.EmailField(blank=True, default='nenhum@provedor.com', unique=True),
+        ),
+        migrations.AddField(
+            model_name='representante',
+            name='profissao',
+            field=models.CharField(blank=True, max_length=255),
+        ),
+        migrations.AlterField(
+            model_name='representante',
+            name='cep',
+            field=models.CharField(max_length=10),
+        ),
+        migrations.AlterField(
+            model_name='representante',
+            name='numero',
+            field=models.IntegerField(default=0),
         ),
     ]
